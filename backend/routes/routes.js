eef47961059d10b6b8eadb1a4cd5c3f483e4dfc1
xen <142533998+Captain-Xen@users.noreply.github.com>
2024-07-07 13:41:52 -0500

    REDO

--- backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const studentController = require('../controllers/studentController');
-
-router.post('/students', studentController.addStudent);
-
-// add other routes if needed
-
-module.exports = router;
